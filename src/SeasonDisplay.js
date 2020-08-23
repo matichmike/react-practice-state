b8530de5ecc3d32aa89485eefeb8968ef6a0f0e4
Michael Matich <matich.mike@gmail.com>
2020-08-22 23:22:33 -0400

    proxy added/cors issue fixed

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  Summer: {
+    text: "Let's hit the beach",
+    iconName: 'sun'
+  },
+  Winter: {
+    text: "Burr,it is chilly",
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "Summer": "Winter";
@@ -10,9 +21,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName }= seasonConfig[season]
 
   return (
-  <div>{season}</div>
+  <div>
+    <i className={`${iconName} icon`} />
+    <h1>{text}</h1>
+    <i className={`${iconName} icon`} />
+  </div>
   );
 };
 
